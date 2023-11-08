d751f8f061a20b67d190967d8f005acfc240d125
Gianpietro Fontana <81692678+gianpietr0@users.noreply.github.com>
2023-11-08 10:40:04 +0100

    Add files via upload

---
+++ knowledgeBase/turismFacts.pl
@@ -0,0 +1,121 @@
+:- discontiguous prop/3.
+attraction('Borough Market').
+attraction('British Museum').
+attraction('Buckingham Palace').
+attraction('Camden Market').
+attraction('Churchill War Rooms Museum').
+attraction('Covent Garden').
+attraction('Design Museum').
+attraction('Greenwich Observatory and the Prime Meridian').
+attraction('Hampstead Heath').
+attraction('Hyde Park').
+attraction('Imperial War Museum').
+attraction('Kew Gardens Royal Botanic Gardens').
+attraction('London Eye').
+attraction('London Zoo').
+attraction('Madame Tussauds Wax Museum').
+attraction('Natural History Museum').
+attraction('Notting Hill and Portobello Road Market').
+attraction('Palace of Westminster Parliament').
+attraction('Royal Opera House').
+attraction('Science Museum').
+attraction('Shakespeares Globe Theatre').
+attraction('Sherlock Holmes Museum').
+attraction('St Pauls Cathedral').
+attraction('Tate Modern').
+attraction('Tower Bridge').
+attraction('Tower of London').
+attraction('Trafalgar Square').
+attraction('Victoria and Albert Museum').
+attraction('West End Theatre District Leicester Square').
+attraction('ZSL London Aquarium').
+prop('Borough Market','latitude',51.504514).
+prop('Borough Market','longitude',-0.090198).
+prop('Borough Market','neighbourhood','Southwark').
+prop('British Museum','latitude',51.519406).
+prop('British Museum','longitude',-0.127643).
+prop('British Museum','neighbourhood','Camden').
+prop('Buckingham Palace','latitude',51.501364).
+prop('Buckingham Palace','longitude',-0.141889).
+prop('Buckingham Palace','neighbourhood','Westminster').
+prop('Camden Market','latitude',51.541341).
+prop('Camden Market','longitude',-0.1476).
+prop('Camden Market','neighbourhood','Camden').
+prop('Churchill War Rooms Museum','latitude',51.502101).
+prop('Churchill War Rooms Museum','longitude',-0.129175).
+prop('Churchill War Rooms Museum','neighbourhood','Westminster').
+prop('Covent Garden','latitude',51.511731).
+prop('Covent Garden','longitude',-0.12327).
+prop('Covent Garden','neighbourhood','Camden').
+prop('Design Museum','latitude',51.47274).
+prop('Design Museum','longitude',-0.185164).
+prop('Design Museum','neighbourhood','Kensington and Chelsea').
+prop('Greenwich Observatory and the Prime Meridian','latitude',51.477928).
+prop('Greenwich Observatory and the Prime Meridian','longitude',0.000716).
+prop('Greenwich Observatory and the Prime Meridian','neighbourhood','Greenwich').
+prop('Hampstead Heath','latitude',51.560731).
+prop('Hampstead Heath','longitude',-0.165782).
+prop('Hampstead Heath','neighbourhood','Camden').
+prop('Hyde Park','latitude',51.50735).
+prop('Hyde Park','longitude',-0.1651).
+prop('Hyde Park','neighbourhood','Westminster').
+prop('Imperial War Museum','latitude',51.495968).
+prop('Imperial War Museum','longitude',-0.108423).
+prop('Imperial War Museum','neighbourhood','Lambeth').
+prop('Kew Gardens Royal Botanic Gardens','latitude',51.478744).
+prop('Kew Gardens Royal Botanic Gardens','longitude',-0.296866).
+prop('Kew Gardens Royal Botanic Gardens','neighbourhood','Richmond upon Thames').
+prop('London Eye','latitude',51.503324).
+prop('London Eye','longitude',-0.119543).
+prop('London Eye','neighbourhood','Lambeth').
+prop('London Zoo','latitude',51.535178).
+prop('London Zoo','longitude',-0.153527).
+prop('London Zoo','neighbourhood','Camden').
+prop('Madame Tussauds Wax Museum','latitude',51.5225).
+prop('Madame Tussauds Wax Museum','longitude',-0.156836).
+prop('Madame Tussauds Wax Museum','neighbourhood','Westminster').
+prop('Natural History Museum','latitude',51.496753).
+prop('Natural History Museum','longitude',-0.176634).
+prop('Natural History Museum','neighbourhood','Kensington and Chelsea').
+prop('Notting Hill and Portobello Road Market','latitude',51.516262).
+prop('Notting Hill and Portobello Road Market','longitude',-0.208031).
+prop('Notting Hill and Portobello Road Market','neighbourhood','Kensington and Chelsea').
+prop('Palace of Westminster Parliament','latitude',51.499633).
+prop('Palace of Westminster Parliament','longitude',-0.124755).
+prop('Palace of Westminster Parliament','neighbourhood','Westminster').
+prop('Royal Opera House','latitude',51.51347).
+prop('Royal Opera House','longitude',-0.121998).
+prop('Royal Opera House','neighbourhood','Camden').
+prop('Science Museum','latitude',51.497992).
+prop('Science Museum','longitude',-0.174436).
+prop('Science Museum','neighbourhood','Kensington and Chelsea').
+prop('Shakespeares Globe Theatre','latitude',51.5076).
+prop('Shakespeares Globe Theatre','longitude',-0.097607).
+prop('Shakespeares Globe Theatre','neighbourhood','Southwark').
+prop('Sherlock Holmes Museum','latitude',51.523685).
+prop('Sherlock Holmes Museum','longitude',-0.158964).
+prop('Sherlock Holmes Museum','neighbourhood','Westminster').
+prop('St Pauls Cathedral','latitude',51.513845).
+prop('St Pauls Cathedral','longitude',-0.098351).
+prop('St Pauls Cathedral','neighbourhood','City of London').
+prop('Tate Modern','latitude',51.507774).
+prop('Tate Modern','longitude',-0.099597).
+prop('Tate Modern','neighbourhood','Southwark').
+prop('Tower Bridge','latitude',51.505456).
+prop('Tower Bridge','longitude',-0.075356).
+prop('Tower Bridge','neighbourhood','Southwark').
+prop('Tower of London','latitude',51.508112).
+prop('Tower of London','longitude',-0.075949).
+prop('Tower of London','neighbourhood','City of London').
+prop('Trafalgar Square','latitude',51.507987).
+prop('Trafalgar Square','longitude',-0.128048).
+prop('Trafalgar Square','neighbourhood','Westminster').
+prop('Victoria and Albert Museum','latitude',51.496639).
+prop('Victoria and Albert Museum','longitude',-0.17218).
+prop('Victoria and Albert Museum','neighbourhood','Kensington and Chelsea').
+prop('West End Theatre District Leicester Square','latitude',51.510707).
+prop('West End Theatre District Leicester Square','longitude',-0.130762).
+prop('West End Theatre District Leicester Square','neighbourhood','Westminster').
+prop('ZSL London Aquarium','latitude',51.5018).
+prop('ZSL London Aquarium','longitude',-0.1195).
+prop('ZSL London Aquarium','neighbourhood','Lambeth').
